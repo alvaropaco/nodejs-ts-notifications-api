8f07003ca7beb24ba968e285fbd3e55dbeede7bc
Álvaro Paçó <alvaropaconeto@gmail.com>
2024-04-15 19:32:26 -0300

    feat: adding security layer

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import helmet from 'helmet';
 import http from 'http';
 import { setupSwagger } from '../swaggerOptions';
 import notificationRoutes from './routes/notification.routes';
@@ -9,6 +10,14 @@ export class Server {
 
   constructor() {
     this._app = express();
+    // Using Helmet to set secure HTTP headers
+    this._app.use(helmet({
+      contentSecurityPolicy: {
+        directives: {
+          "script-src": ["'self'"],
+        },
+      },
+    }));
     this._app.use(express.json());
 
     this._app.use('/notifications', notificationRoutes);
